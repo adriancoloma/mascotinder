a4f425da23a77942fe4cf12a9457dd54166cea47
adriancoloma <carlos.coloma@epn.edu.ec>
2022-08-22 21:20:20 -0500

    Se agrego una clase para probar la capa de datos

---
+++ mascotinder/src/main/java/TestJPA.java
@@ -0,0 +1,31 @@
+import mascotinder.modelo.dao.DAOFactory;
+import mascotinder.modelo.dao.MascotaDAO;
+import mascotinder.modelo.dao.PersonaDAO;
+import mascotinder.modelo.entidades.Mascota;
+import mascotinder.modelo.entidades.Persona;
+
+public class TestJPA {
+
+	public static void main(String[] args) {
+		DAOFactory dao = DAOFactory.getFactory();
+		
+		PersonaDAO personaDAO = dao.crearPersonaDAO();
+		Persona persona = new Persona();
+		persona.setNombre("Adrian");
+		persona.setPassword("adrian123");
+		personaDAO.create(persona);
+		
+		MascotaDAO mascotaDAO = dao.crearMascotaDAO();
+		Mascota mascota = new Mascota();
+		mascota.setNombre("Rufus");
+		mascota.setEdad(5);
+		mascota.setDueno(persona);
+		mascotaDAO.create(mascota);
+		
+		mascotaDAO.getAll();
+		
+		
+
+	}
+
+}
