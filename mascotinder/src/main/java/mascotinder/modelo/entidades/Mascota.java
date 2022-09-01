4af11658396d4bcca0c6a57f1bd82472a252c167
adriancoloma <carlos.coloma@epn.edu.ec>
2022-09-01 15:06:17 -0500

    Preferencias añadidas a la entidad Mascota

--- mascotinder/src/main/java/mascotinder/modelo/entidades/Mascota.java
+++ mascotinder/src/main/java/mascotinder/modelo/entidades/Mascota.java
@@ -33,6 +33,14 @@ public class Mascota implements Serializable{
 	private String pathImagen2;
 	@Column(name = "imagen3")
 	private String pathImagen3;
+	@Column(name = "preferencia_tipo")
+	private String preferenciaTipo;
+	@Column(name = "preferencia_sexo")
+	private char preferenciaSexo;
+	@Column(name = "preferencia_maximo_edad")
+	private int preferenciaMaximoEdad;
+	@Column(name = "preferencia_minimo_edad")
+	private int preferenciaMinimoEdad;
 	
 	@JoinColumn(name = "dueno_id")
 	@ManyToOne
@@ -120,7 +128,25 @@ public class Mascota implements Serializable{
 		
 		return pathImagenes;
 	}
+	public String getPreferenciaTipo() {
+		return preferenciaTipo;
+	}
+	public char getPreferenciaSexo() {
+		return preferenciaSexo;
+	}
+	public int getPreferenciaMaximoEdad() {
+		return preferenciaMaximoEdad;
+	}
+	public int getPreferenciaMinimoEdad() {
+		return preferenciaMinimoEdad;
+	}
 	
+	public void setPreferencias(String tipo, char sexo, int edadMinima, int edadMaxima) {
+		this.preferenciaTipo = tipo;
+		this.preferenciaSexo = sexo;
+		this.preferenciaMinimoEdad = edadMinima;
+		this.preferenciaMaximoEdad = edadMaxima;
+	}
 	
 	
 	
