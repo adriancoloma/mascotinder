90d0e753775425af74c3879b0fb16c5f35400ff3
Jefferson Alquinga <55715332+Jeffers1998@users.noreply.github.com>
2022-09-03 17:44:05 -0500

    Me gusta y no Me gusta controller

--- mascotinder/src/main/java/mascotinder/controladores/NoMeGustaController.java
+++ mascotinder/src/main/java/mascotinder/controladores/NoMeGustaController.java
@@ -6,34 +6,63 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import mascotinder.modelo.dao.DAOFactory;
+import mascotinder.modelo.entidades.Mascota;
+import mascotinder.modelo.entidades.Match;
+
 /**
  * Servlet implementation class NoMeGustaController
  */
 public class NoMeGustaController extends HttpServlet {
 	private static final long serialVersionUID = 1L;
        
-    /**
-     * @see HttpServlet#HttpServlet()
-     */
+ 
     public NoMeGustaController() {
         super();
-        // TODO Auto-generated constructor stub
+    
     }
 
-	/**
-	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
-	 */
+
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
-		response.getWriter().append("Served at: ").append(request.getContextPath());
+		
+		
+		//Obtener atributos
+				int idMascota1 = Integer.parseInt(request.getParameter("idMascota1"));
+				int idMascota2 = Integer.parseInt(request.getParameter("idMascota2"));
+				
+				 Match nuevoMatch = new Match();
+
+		        // Llamar al modelo 
+				Mascota Mascota1 = DAOFactory.getFactory().crearMascotaDAO().getById(idMascota1);
+				Mascota Mascota2 = DAOFactory.getFactory().crearMascotaDAO().getById(idMascota2);
+				
+				nuevoMatch.setMascota1(Mascota1);
+				
+				nuevoMatch.setMascota2(Mascota2);
+			    
+			
+			    
+			    if(Mascota1 != Mascota2 ) {
+			    nuevoMatch.setMeGusta(true);
+			    
+			    
+			    DAOFactory.getFactory().crearMatchDAO().create(nuevoMatch);
+			    
+			    }else{
+			    	//Enviar a la vista 
+				    
+			    	response.sendRedirect("jsp/verCatalogo.jsp");
+
+			    }
+					
+				
+				
+				
 	}
 
-	/**
-	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
-	 */
+
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
-		doGet(request, response);
+	
 	}
 
 }
