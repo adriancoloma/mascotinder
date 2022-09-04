967bf6737fc18c001808bc05392606a90e3f16f4
adriancoloma <carlos.coloma@epn.edu.ec>
2022-09-04 09:19:31 -0500

    Metodo getMatchs añadido

--- mascotinder/src/main/java/mascotinder/modelo/jpadao/JPAMatchDAO.java
+++ mascotinder/src/main/java/mascotinder/modelo/jpadao/JPAMatchDAO.java
@@ -1,7 +1,10 @@
 package mascotinder.modelo.jpadao;
 
+import java.util.List;
+
 import mascotinder.modelo.dao.GenericDAO;
 import mascotinder.modelo.dao.MatchDAO;
+import mascotinder.modelo.entidades.Mascota;
 import mascotinder.modelo.entidades.Match;
 
 public class JPAMatchDAO extends JPAGenericDAO<Match, Integer> implements MatchDAO{
@@ -9,5 +12,8 @@ public class JPAMatchDAO extends JPAGenericDAO<Match, Integer> implements MatchD
 	public JPAMatchDAO() {
 		super(Match.class);
 	}
-
+	
+	public List<Mascota> getMatchs(Mascota mascota){
+		return null;
+	}
 }
