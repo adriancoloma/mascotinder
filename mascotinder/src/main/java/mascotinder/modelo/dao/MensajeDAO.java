88a800e9ef655909e14c00a2c8295cfc6e84d6f1
adriancoloma <carlos.coloma@epn.edu.ec>
2022-09-04 10:35:13 -0500

    Estructurada creada para el caso de uso Chat

--- mascotinder/src/main/java/mascotinder/modelo/dao/MensajeDAO.java
+++ mascotinder/src/main/java/mascotinder/modelo/dao/MensajeDAO.java
@@ -1,5 +1,11 @@
 package mascotinder.modelo.dao;
 
-public interface MensajeDAO {
+import mascotinder.modelo.entidades.Persona;
 
+import java.util.List;
+
+import mascotinder.modelo.entidades.Mensaje;
+
+public interface MensajeDAO {
+	public List<Mensaje> getMensajes(Persona emisor, Persona receptor);
 }
--- mascotinder/src/main/java/mascotinder/modelo/entidades/Mensaje.java
+++ mascotinder/src/main/java/mascotinder/modelo/entidades/Mensaje.java
@@ -1,5 +1,27 @@
 package mascotinder.modelo.entidades;
 
 public class Mensaje {
-
+	private Persona emisor;
+	private Persona destinatario;
+	private String contenido;
+	
+	
+	public Mensaje(Persona emisor, Persona destinatario, String contenido) {
+		super();
+		this.emisor = emisor;
+		this.destinatario = destinatario;
+		this.contenido = contenido;
+	}
+	
+	public Persona getEmisor() {
+		return emisor;
+	}
+	public Persona getDestinatario() {
+		return destinatario;
+	}
+	public String getContenido() {
+		return contenido;
+	}
+	
+	
 }
